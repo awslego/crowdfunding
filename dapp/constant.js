a9539c56732253b51bb709e8b288417552d3cfad
goblinok <goblinok@gmail.com>
2018-08-13 16:02:06 +0900

    init

--- dapp/constant.js
+++ dapp/constant.js
@@ -1,5 +1,5 @@
 exports.mainAddress = "0xf066f4c87085d4bc52bfe02622d428f33bec48a3";                // 토큰 생성 계정
 exports.makerAddress = "0xef77e0895435f4324cc724cb454fc14d06bc392e";               // 제작자 계정
 exports.eoaAddress = "0xdbb5a9a5eeca3c667459b64a638bcf34a4b7fbe9";                 // 참여자 계정
-exports.TokenContractAddress = "0x404a97c9fd2ebc7c3caf4fb7fe47214bb3e6494a";       // 토큰 계정
-exports.crowdFundContractAddress = "0x24ea42504e41eb8d331864a6cc81fbb5b8a0d5a5";    // 펀딩 계정
+exports.TokenContractAddress = "0x0ab73bdd6f8ebab0aaea0c7dee7b2b01d3d4fdc3";       // 토큰 계정
+exports.crowdFundContractAddress = "0xabd4fdae6fa80d624ef5ba66b31b3adfd8cf446e";    // 펀딩 계정
